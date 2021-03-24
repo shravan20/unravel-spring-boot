5cb32ee32791e0c10290c430e4c1588864d3422c
shravan20 <mrshravankumarb@gmail.com>
2021-03-24 18:36:12 +0530

    :sparkles: ArrayList[]

--- src/main/java/com/example/UnravelSpringBoot/UserController/UserController.java
+++ src/main/java/com/example/UnravelSpringBoot/UserController/UserController.java
@@ -1,7 +1,11 @@
 package com.example.UnravelSpringBoot.UserController;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 
@@ -9,8 +13,18 @@ public class UserController {
 
 
     @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        //LinkedList<String> = new LinkedList<String>();
+        return "Hello " + name;
     }
 
+    @GetMapping("/ArrayList")
+    public ArrayList list() {
+        ArrayList<String> list=new ArrayList<String>();//Creating arraylist
+        list.add("Mango");//Adding object in arraylist
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+        return list;
+    }
 }
