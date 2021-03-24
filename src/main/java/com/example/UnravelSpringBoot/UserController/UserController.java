466d34260b87283eea23aef3774c4b2af5968879
shravan20 <mrshravankumarb@gmail.com>
2021-03-24 18:57:19 +0530

    :sparkles: LinkedList[] | List[]

--- src/main/java/com/example/UnravelSpringBoot/UserController/UserController.java
+++ src/main/java/com/example/UnravelSpringBoot/UserController/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 
@@ -19,9 +21,31 @@ public class UserController {
     }
 
     @GetMapping("/ArrayList")
-    public ArrayList list() {
+    public ArrayList arraylist() {
         ArrayList<String> list=new ArrayList<String>();//Creating arraylist
         list.add("Mango");//Adding object in arraylist
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+        return list;
+    }
+
+    @GetMapping("/LinkedList")
+    public LinkedList linkedlist() {
+        LinkedList<String> al=new LinkedList<String>();
+        al.add("Ravi");
+        al.add("Vijay");
+        al.add("Ravi");
+        al.add("Ajay");
+        System.out.print(al);
+        return al;
+    }
+
+    @GetMapping("/List")
+    public List<String> clist() {
+        List<String> list = new ArrayList<String>();
+        //Adding elements in the List
+        list.add("Mango");
         list.add("Apple");
         list.add("Banana");
         list.add("Grapes");
